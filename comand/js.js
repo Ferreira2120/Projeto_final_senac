80fdbfd227c70c6d4a545bbeb95a9b9c4ee108ac
PEDRO FERREIRA GOMES <pedro.fgomes2@senacsp.edu.br>
2024-11-12 16:04:48 -0300

    arrumando responsividade do site

--- comand/js.js
+++ comand/js.js
@@ -2,14 +2,17 @@ const openOffCanva = document.querySelector(".btn_offcanvas");
 const offcanva = document.querySelector(".offcanvas");
 const bodyResponsivo = document.querySelector("body");
 const menuCabeca = document.querySelector(".primeira_ul")
+const menu_inativo = document.querySelector(".primeira_ul.inativo");
+
 
 function abrirOffcanva() {
   offcanva.classList.add("open");
 }
 
 openOffCanva.addEventListener("click", abrirOffcanva);
+
 function resposividade(){
-  if(bodyResponsivo <= 550px){
+  if(bodyResponsivo <= ){
     menuCabeca.classList.add('inativo')
   }
 }
