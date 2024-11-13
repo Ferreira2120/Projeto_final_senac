cae012214c83fde22484d4e60c3b682c95936768
unknown <pedroferreira.gomes03@gmail.com>
2024-11-12 23:17:46 -0300

    add nav no projeto e fanalizando menu

--- comand/js.js
+++ comand/js.js
@@ -1,16 +1,10 @@
 const openOffCanva = document.querySelector(".btn_offcanvas");
 const offcanva = document.querySelector(".offcanvas");
 const bodyResponsivo = document.querySelector("body");
-const menuCabeca = document.querySelector(".primeira_ul")
+const menuCabeca = document.querySelector(".primeira_ul");
 
 function abrirOffcanva() {
   offcanva.classList.add("open");
 }
 
 openOffCanva.addEventListener("click", abrirOffcanva);
-function resposividade(){
-  if(bodyResponsivo <= 550px){
-    menuCabeca.classList.add('inativo')
-  }
-}
-
